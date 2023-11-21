4ba643e5211ed43469f287b85aca59ac9b21bed6
ChaosO9 <irfannoorh@gmail.com>
2023-11-21 16:35:57 +0800

    Auth API v1

---
+++ auth-api/models/index.js
@@ -0,0 +1,31 @@
+import { Sequelize, DataTypes } from 'sequelize';
+import * as dbConfig from '../config/config.js';
+import users from './users.js';
+
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    operatorsAliases: false,
+});
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connected to database!');
+    })
+    .catch((err) => {
+        console.log('Error' + err);
+    });
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.users = users(sequelize, DataTypes);
+
+db.sequelize.sync({ force: false }).then(() => {
+    console.log('re-sync done!');
+});
+
+export default db;
