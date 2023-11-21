32416ba54a8d7017516d763925b9aa1aecdf796e
ChaosO9 <irfannoorh@gmail.com>
2023-11-21 20:33:03 +0800

    Change Dockerfile

--- auth-api/server.js
+++ auth-api/server.js
@@ -6,7 +6,7 @@ import { routes } from "./route.js";
 const init = async () => {
 
     const server = Hapi.server({
-        port: 3000,
+        port: process.env.PORT || 8080,
         host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0"
     });
 
