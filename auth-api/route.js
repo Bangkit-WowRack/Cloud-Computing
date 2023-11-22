3e6a0a32aabc6e56e1f334a00ec117719f59e19e
ChaosO9 <94062949+ChaosO9@users.noreply.github.com>
2023-11-22 20:32:26 +0800

    Delete auth-api directory

--- auth-api/route.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import { getOAuthToken } from "./handler.js";
-
-export const routes = [{
-    method: 'POST',
-    path: '/v1/api/gateway/user/auth',
-    handler: getOAuthToken
-}]
